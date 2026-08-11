de228a6b277364c23a7f128104049a1fcae49258
Mr-Josh <jkimmich839@gmail.com>
2026-08-11 04:55:15 +0100

    feat(dev): enforce protected developer shell

--- frontend/src/dev/DevApp.jsx
+++ frontend/src/dev/DevApp.jsx
@@ -3,16 +3,19 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import DevDashboard from './DevDashboard';
 import DevLayout from './DevLayout';
 import DevUsers from './DevUsers';
+import ProtectedDevRoute from './ProtectedDevRoute';
 import './dev.css';
 
 export default function DevApp() {
   return (
     <Routes>
-      <Route element={<DevLayout />}>
-        <Route path="/dev" element={<DevDashboard />} />
-        <Route path="/dev/users" element={<DevUsers />} />
+      <Route element={<ProtectedDevRoute />}>
+        <Route element={<DevLayout />}>
+          <Route path="/dev" element={<DevDashboard />} />
+          <Route path="/dev/users" element={<DevUsers />} />
+        </Route>
       </Route>
-      <Route path="*" element={<Navigate to="/dev" replace />} />
+      <Route path="*" element={<Navigate to="/admin/login" replace />} />
     </Routes>
   );
 }
