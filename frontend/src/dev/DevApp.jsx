87947cae2e6070cb7eccc19a1d3cf84b3c73b524
Mr-Josh <jkimmich839@gmail.com>
2026-08-11 05:44:20 +0100

    feat(dev): expand developer console routes

--- frontend/src/dev/DevApp.jsx
+++ frontend/src/dev/DevApp.jsx
@@ -3,6 +3,10 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import DevDashboard from './DevDashboard';
 import DevLayout from './DevLayout';
 import DevUsers from './DevUsers';
+import DevDatabase from './DevDatabase';
+import DevSecurity from './DevSecurity';
+import DevAudit from './DevAudit';
+import DevSystem from './DevSystem';
 import ProtectedDevRoute from './ProtectedDevRoute';
 import './dev.css';
 
@@ -13,6 +17,10 @@ export default function DevApp() {
         <Route element={<DevLayout />}>
           <Route path="/dev" element={<DevDashboard />} />
           <Route path="/dev/users" element={<DevUsers />} />
+          <Route path="/dev/database" element={<DevDatabase />} />
+          <Route path="/dev/security" element={<DevSecurity />} />
+          <Route path="/dev/audit" element={<DevAudit />} />
+          <Route path="/dev/system" element={<DevSystem />} />
         </Route>
       </Route>
       <Route path="*" element={<Navigate to="/admin/login" replace />} />
