bd3905f0bf56a9811c217378e92ed2cc0bbe51d1
Mr-Josh <jkimmich839@gmail.com>
2026-08-19 10:56:16 +0100

    refactor(site): remove public pastor ministries sermons gallery contact routes

--- frontend/src/site/PublicApp.jsx
+++ frontend/src/site/PublicApp.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { Footer, Header } from './components';
-import { Home, About, Pastor, Ministries, Programs, Events, Sermons, Gallery } from './pages/ContentPages';
-import { Testimonials, Prayer, Evangelism, Help, Contact, Donate } from './pages/InteractionPages';
+import { Home, About, Programs, Events } from './pages/ContentPages';
+import { Testimonials, Prayer, Evangelism, Help, Donate } from './pages/InteractionPages';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -23,17 +23,12 @@ export default function PublicApp() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/pastor" element={<Pastor />} />
-          <Route path="/ministries" element={<Ministries />} />
           <Route path="/programs" element={<Programs />} />
           <Route path="/events" element={<Events />} />
-          <Route path="/sermons" element={<Sermons />} />
-          <Route path="/gallery" element={<Gallery />} />
           <Route path="/testimonials" element={<Testimonials />} />
           <Route path="/prayer" element={<Prayer />} />
           <Route path="/evangelism" element={<Evangelism />} />
           <Route path="/help" element={<Help />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/donate" element={<Donate />} />
           <Route path="*" element={<Home />} />
         </Routes>
