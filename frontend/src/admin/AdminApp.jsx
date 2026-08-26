f33a1b2cc174cc12652cc96db45201e0cdd55a89
Mr-Josh <jkimmich839@gmail.com>
2026-08-26 09:00:06 +0100

    Enable event management in admin

--- frontend/src/admin/AdminApp.jsx
+++ frontend/src/admin/AdminApp.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import AdminLogin from './AdminLogin';
 import AdminDashboard from './AdminDashboard';
@@ -18,14 +17,9 @@ export default function AdminApp() {
       <Route element={<ProtectedAdminRoute />}>
         <Route element={<AdminLayout />}>
           <Route path="/admin" element={<AdminDashboard />} />
-          {/* <Route path="/admin/prayer-requests" element={<AdminRequestsPage resource="prayer-requests" />} /> */}
-          {/* <Route path="/admin/help-requests" element={<AdminRequestsPage resource="help-requests" />} /> */}
           <Route path="/admin/testimonials" element={<AdminResourcePage resource="testimonials" />} />
           <Route path="/admin/donations" element={<AdminDonationsPage />} />
-          {/* <Route path="/admin/content" element={<AdminContentPage />} /> */}
-          {/* <Route path="/admin/ministries" element={<AdminResourcePage resource="ministries" />} /> */}
-          {/* <Route path="/admin/programs" element={<AdminResourcePage resource="programs" />} /> */}
-          {/* <Route path="/admin/events" element={<AdminResourcePage resource="events" />} /> */}
+          <Route path="/admin/events" element={<AdminResourcePage resource="events" />} />
           <Route path="/admin/users" element={<AdminUsers />} />
           <Route path="/admin/settings" element={<AdminSettings />} />
         </Route>
