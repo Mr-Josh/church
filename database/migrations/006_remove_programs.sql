89cd8347ea205096986f991c2778afd5cfe8465b
Mr-Josh <jkimmich839@gmail.com>
2026-08-26 11:30:15 +0100

    db: remove programmes module

---
+++ database/migrations/006_remove_programs.sql
@@ -0,0 +1,5 @@
+USE church;
+
+-- Programs are no longer part of the public ministry site.
+-- Remove the table from existing databases as part of the module removal.
+DROP TABLE IF EXISTS programs;
